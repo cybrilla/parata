3a435ae9eb03c500ca8b3427802b00adcbcd8326
Swaroop SM <swaroop@cybrilla.com>
2015-06-24 21:01:56 +0530

    removeed exisiting parser test cases because change in the parser api

--- test/lib/parser.test.js
+++ test/lib/parser.test.js
@@ -3,32 +3,6 @@ var expect = require('chai').expect,
     path = require('path'),
     parser = require('../../lib/parser.js');
 
-describe('Comments Parser', function(){
-  var styleContents = fs.readFileSync(path.join(__dirname, '../fixtures/style.sass')),
-      exampleContents = fs.readFileSync(path.join(__dirname, '../fixtures/example.html')),
-      results;
+describe('Parser', function(){
 
-  before(function() {
-    results = parser.parseCommentBlock(styleContents, exampleContents);
-  });
-
-  it("returns the correct size for results", function() {
-    expect(results.length).to.equal(2);
-  });
-
-  it("returns the correct data for a component", function() {
-    var component = results[0],
-        expected = {
-          name: 'component-name-1',
-          description: 'I am an awesome component 1.',
-          example: '\n<div class=\"component-name-1\"></div>\n',
-          classNames: [
-            'primary',
-            'default',
-            'secondary'
-          ]
-        };
-
-    expect(component).to.deep.equal(expected);
-  });
 });
