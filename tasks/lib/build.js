ce6fcfbe800d2709f217734e9e8d40d8a4cd65f0
Ankit Famyial <ankit.famiyal@cybrilla.com>
2015-08-11 17:31:35 +0530

    extracted setting contents method out and added tests for parser

--- tasks/lib/build.js
+++ tasks/lib/build.js
@@ -34,7 +34,9 @@ var getComponents = function(srcPath, styleExt, dest) {
     if(stat.isDirectory()) {
       parser = new Parser();
       parser.setStyleFilePath(path.join(item, 'style.' + styleExt))
+            .setsetStyleContents()
             .setExampleFilePath(path.join(item, 'example.html'))
+            .setExampleContents()
             .run();
 
       results = parser.getComponents();
--- test/lib/parser.test.js
+++ test/lib/parser.test.js
@@ -1,8 +1,113 @@
 var expect = require('chai').expect,
     fs = require('fs-extra'),
     path = require('path'),
-    parser = require('../../lib/parser.js');
+    Parser = require('../../lib/parser.js');
 
 describe('Parser', function(){
 
+  var parser = new Parser(),
+      exampleFilePath = path.join(__dirname, '../fixtures/example.html'),
+      styleFilePath = path.join(__dirname, '../fixtures/style.sass');
+
+  describe('#setExampleFilePath', function() {
+    it('returns object of type Parser', function() {
+      expect(parser.setExampleFilePath(exampleFilePath)).to.equal(parser);
+    });
+  });
+
+  describe('#setExampleContents', function() {
+    it('returns object of type Parser', function() {
+      expect(parser.setExampleContents()).to.equal(parser);
+    });
+  });
+
+  describe('#setStyleFilePath', function() {
+    it('returns object of type Parser', function() {
+      expect(parser.setStyleFilePath(styleFilePath)).to.equal(parser);
+    });
+  });
+
+  describe('#setStyleContents', function() {
+    it('returns object of type Parser', function() {
+      expect(parser.setStyleContents()).to.equal(parser);
+    });
+  });
+
+  describe('parsing methods', function() {
+
+    var pattern = /\/\*\*([\s\S]+?)\*\//gi,
+        styleContents = fs.readFileSync(styleFilePath, 'utf8'),
+        match = pattern.exec(styleContents),
+        emptyContents = '',
+        emptyMatch = pattern.exec(emptyContents);
+
+    describe('#parseName', function() {
+      it('should parse the name correctly', function() {
+        expect(parser.parseName(match)).to.equal('component-name-1');
+      });
+
+      it('should return empty in case of no match', function() {
+        expect(parser.parseName(emptyMatch)).to.be.empty;
+      });
+    });
+
+    describe('#parseDescription', function() {
+      it('should parse the description correctly', function() {
+        expect(parser.parseDescription(match)).to.equal('I am an awesome component 1.');
+      });
+
+      it('should return empty in case of no match', function() {
+        expect(parser.parseDescription(emptyMatch)).to.be.empty;
+      });
+    });
+
+    describe('#parseVariants', function() {
+      it('should parse the variants correctly', function() {
+        expect(parser.parseVariants(match)).to.have.length(3);
+        expect(parser.parseVariants(match)).to.include('primary', 'default', 'secondary');
+      });
+
+      it('should return empty in case of no match', function() {
+        expect(parser.parseVariants(emptyMatch)).to.be.empty;
+      });
+    });
+
+    describe('#parseHTMLExample', function() {
+      it('should parse the HTML correctly', function() {
+        expect(parser.parseHTMLExample('component-name-1')).to.be.eq('\n<div class="component-name-1"></div>\n');
+      });
+
+      it('should return empty in case of no match', function() {
+        expect(parser.parseHTMLExample(emptyMatch)).to.be.empty;
+      });
+    });
+
+    describe('#parseJSExample', function() {
+      it('should parse the JS correctly', function() {
+        expect(parser.parseJSExample('component-name-1')).to.be.eq('\ndocument.write("Example Script")\n');
+      });
+
+      it('should return empty in case of no match', function() {
+        expect(parser.parseJSExample(emptyMatch)).to.be.empty;
+      });
+    });
+
+  });
+
+  describe('#getComponents', function() {
+    it('returns empty for no existing components', function() {
+      expect(parser.getComponents()).to.be.empty;
+    });
+
+    it('returns existing components if present', function() {
+      parser.run();
+      expect(parser.getComponents()).to.have.length(1);
+    });
+  });
+
+  describe('#run', function() {
+    it('generates and returns the all the components', function() {
+      expect(parser.run()).to.have.length(2);
+    });
+  });
 });
